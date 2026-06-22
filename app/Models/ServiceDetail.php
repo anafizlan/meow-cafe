2a095a6a0038dce28f7986c98d6195e068bfe34f
anafizlan <anafizlan01@gmail.com>
2026-06-22 14:58:48 +0800

    Create ServiceDetail model

---
+++ app/Models/ServiceDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceDetail extends Model
+{
+    protected $table = 'service_details';
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'short_desc',
+        'image',
+        'about_title',
+        'about_desc',
+        'features',
+        'pricing',
+    ];
+}
